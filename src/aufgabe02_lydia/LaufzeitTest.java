1b862caaef7d77f7c3765a3ad8e7417e474ba2eb
lpflug <lpflug@NTCG57.ntcg.local>
2016-10-05 11:46:16 +0200

    Fehlersuche Array add()

--- src/aufgabe02_lydia/LaufzeitTest.java
+++ src/aufgabe02_lydia/LaufzeitTest.java
@@ -26,9 +26,29 @@ public class LaufzeitTest {
 //		System.out.println(pufferLinkedList.kapazitaet);
 //		System.out.println(pufferLinkedList.inhalt());
 		
+		System.out.println("________________________");
+		
 //		Array
 		
-		
+		PufferArray pufferArray = new PufferArrayImpl(5);
+		pufferArray.add(1);
+		System.out.println(pufferArray);
+		pufferArray.add(2);
+		System.out.println(pufferArray);
+		pufferArray.remove(1);
+		System.out.println(pufferArray);
+		pufferArray.add(3);
+		pufferArray.add(4);
+		pufferArray.add(5);
+		System.out.println(pufferArray);
+		pufferArray.add(6);
+		System.out.println(pufferArray);
+		System.out.println(pufferArray.kapazitaet);
+		System.out.println(pufferArray.inhalt());
+		pufferArray.add(7);
+		System.out.println(pufferArray);
+		System.out.println(pufferArray.kapazitaet);
+		System.out.println(pufferArray.inhalt());
 		
 	}
 	
